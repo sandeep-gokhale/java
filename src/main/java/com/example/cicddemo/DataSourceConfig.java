6fc1511d7b1fb155f781fd3ece6f51566d68a247
sandeep-gokhale <sandeep.gokhale@techvito.in>
2022-11-10 19:43:45 +0530

    Removes Postgres call

--- src/main/java/com/example/cicddemo/DataSourceConfig.java
+++ src/main/java/com/example/cicddemo/DataSourceConfig.java
@@ -1,24 +1,24 @@
-package com.example.cicddemo;
+// package com.example.cicddemo;
 
-import com.zaxxer.hikari.HikariDataSource;
+// import com.zaxxer.hikari.HikariDataSource;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-public class DataSourceConfig {
+// public class DataSourceConfig {
 
-    public static DataSource getDataSource(String dataSourceURL, String dbUserName, String dbPassword,
-                                           String driverClass, String poolName) {
+//     public static DataSource getDataSource(String dataSourceURL, String dbUserName, String dbPassword,
+//                                            String driverClass, String poolName) {
 
-        HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(dataSourceURL);
-        ds.setUsername(dbUserName);
-        ds.setPassword(dbPassword);
-        ds.setDriverClassName(driverClass);
-        ds.setPoolName(poolName);
-        ds.setMinimumIdle(2);
-        ds.setMaxLifetime(600000);
-        ds.setMaximumPoolSize(100);
-        ds.setLeakDetectionThreshold(30000);
-        return ds;
-    }
-}
+//         HikariDataSource ds = new HikariDataSource();
+//         ds.setJdbcUrl(dataSourceURL);
+//         ds.setUsername(dbUserName);
+//         ds.setPassword(dbPassword);
+//         ds.setDriverClassName(driverClass);
+//         ds.setPoolName(poolName);
+//         ds.setMinimumIdle(2);
+//         ds.setMaxLifetime(600000);
+//         ds.setMaximumPoolSize(100);
+//         ds.setLeakDetectionThreshold(30000);
+//         return ds;
+//     }
+// }
