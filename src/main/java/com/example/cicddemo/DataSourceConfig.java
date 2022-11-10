bf9901c4114a54528645a8f24340235771d29aaf
sandeep-gokhale <sandeep.gokhale@techvito.in>
2022-11-10 13:19:05 +0530

    New Changes

---
+++ src/main/java/com/example/cicddemo/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.example.cicddemo;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class DataSourceConfig {
+
+    public static DataSource getDataSource(String dataSourceURL, String dbUserName, String dbPassword,
+                                           String driverClass, String poolName) {
+
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(dataSourceURL);
+        ds.setUsername(dbUserName);
+        ds.setPassword(dbPassword);
+        ds.setDriverClassName(driverClass);
+        ds.setPoolName(poolName);
+        ds.setMinimumIdle(2);
+        ds.setMaxLifetime(600000);
+        ds.setMaximumPoolSize(100);
+        ds.setLeakDetectionThreshold(30000);
+        return ds;
+    }
+}
---
+++ src/main/java/com/example/cicddemo/PostgresConfig.java
@@ -0,0 +1,53 @@
+package com.example.cicddemo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class PostgresConfig extends DataSourceConfig {
+    private static final Logger logger = LogManager.getLogger(PostgresConfig.class);
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String dataSourceURL;
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    @Primary
+
+    public DataSource dataSource() throws SQLException {
+
+        logger.debug("Driver " + driver);
+        logger.debug("DataSourceURL: " + dataSourceURL);
+
+        String dbUserName = env.getProperty("UUNAME");
+        String dbPassword = env.getProperty("paassword");
+
+        System.out.println(dbUserName);
+
+        return getDataSource(dataSourceURL, dbUserName, dbPassword, driver, "POSTGRES_POOL");
+    }
+
+    @DependsOn("jdbcTemplate")
+    @Bean("namedParameterJdbcTemplate")
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) throws SQLException {
+        logger.debug("jdbcTemplate: " + jdbcTemplate);
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+
+}
